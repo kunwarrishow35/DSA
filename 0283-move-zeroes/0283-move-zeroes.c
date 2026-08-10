cd2a61a6b8f7d8629dcf9fc71aa0e24a3ef9a88e
kunwarrishow35 <kunwarrishow35@gmail.com>
2026-08-10 21:29:26 +0530

    Time: 0 ms (100%), Space: 20.4 MB (49.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,23 +1,24 @@
 void moveZeroes(int* nums, int numsSize) {
     int ans[numsSize];
-
-    if(nums == 0)
+    if(nums==0){
         return;
-    int k = 0;
+    }
+    int k =0;
     for(int i=0; i<numsSize; i++){
         if(nums[i] != 0){
             ans[k] = nums[i];
             k++;
         }
     }
-    
     while(k<numsSize){
-        ans[k] =0;
+        ans[k]=0;
         k++;
     }
-    for(int i = 0; i < numsSize; i++)
-    {
-        nums[i] = ans[i];
+
+    for(int j=0; j<numsSize; j++){
+        nums[j] = ans[j];
     }
 
-}
+
+
+}   
