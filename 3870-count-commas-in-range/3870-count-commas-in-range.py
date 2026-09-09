2d2813e954f1cf278aa6ea0754d17bdb88706b53
kunwarrishow35 <kunwarrishow35@gmail.com>
2026-09-09 19:00:33 +0530

    Time: 1 ms (29.52%), Space: 12.3 MB (63.81%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -1,3 +1,8 @@
-class Solution:
-    def countCommas(self, n: int) -> int:
+class Solution(object):
+    def countCommas(self, n):
         return max(n-999, 0)
+        """
+        :type n: int
+        :rtype: int
+        """
+        
